307c3501b828a8634602bb378600a3bd03e7f7ed
liujiana <liujiana@yy.com>
2020-10-14 23:16:10 +0800

    feat: 增加斐波那契数列

---
+++ fib.js
@@ -0,0 +1,45 @@
+/**
+ * 
+ * 斐波那契数列
+ * F(n) = F(n-1) + F(n-2) n > 1
+ * F(0) = 0, F(1) = 1
+ */
+
+// 第一个版本，暴力递归
+function fib(n) {
+  if (n === 1 || n === 2) {
+    return 1
+  }
+  return fib(n - 1) + fib(n - 2)
+}
+
+console.log(fib(4))
+
+// 第二个版本，缓存版递归
+function fib1(n) {
+  let cache = []
+  return helper(cache, n)
+}
+
+function helper(cache, n) {
+  if (n === 1 || n === 2) {
+    return 1
+  }
+  if (cache[n]) return cache[n]
+  cache[n] = helper(cache, n-1) + helper(cache, n-2)
+  return cache[n]
+}
+
+console.log('fib1:', fib1(4))
+
+// 自底向上
+function fib2(n) {
+  let dp = []
+  dp[1] = dp[2] = 1
+  for(let i = 3; i <= n; i++) {
+    dp[i] = dp[i-1] + dp[i-2]
+  }
+  return dp[n]
+}
+
+console.log('fib2:', fib2(4))
