0d773c3fa861ac5348cc4be31976b2ec6afcfb3f
liujiana <liujiana@yy.com>
2020-10-14 13:19:15 +0800

    feat: 增加冒泡排序代码

---
+++ bubbleSort.js
@@ -0,0 +1,18 @@
+// 冒泡排序
+
+function bubbleSort (arr) {
+  for (let i = 0; i < arr.length; i += 1) {
+    for (let j = i + 1; j < arr.length; j += 1) {
+      if (arr[j] < arr[i]) {
+        [ arr[i], arr[j] ] = [ arr[j], arr[i] ]
+      }
+    }
+  }
+  return arr
+}
+
+
+// 测试代码
+const arr = [3,5,6,1,7,9]
+const result = bubbleSort(arr)
+console.log(result)
